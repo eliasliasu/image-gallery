b4503795e0ecf803eac1602b6851bb39269d96dc
Elias Liasu <eliasbabatunde@gmail.com>
2024-07-28 11:15:01 +0100

    simplify component importation in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import ImageCard from './components/ImageCard';
-import Welcome from './components/Welcome';
+import { Header, Search, ImageCard, Welcome } from './components';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as Search } from './Search';
+export { default as ImageCard } from './ImageCard';
+export { default as Welcome } from './Welcome';
