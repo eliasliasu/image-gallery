a18fefb12313b61142d88a0454ec3ca4ca27299f
Elias Liasu <eliasbabatunde@gmail.com>
2024-07-28 10:53:04 +0100

    welcome component

---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Jumbotron, Button } from 'react-bootstrap';
+
+const Welcome = () => {
+  return (
+    <Jumbotron>
+      <h1>Images Gallary!</h1>
+      <p>
+        This is a simple application the retrieves phoos using unsplash api. To
+        start enter any search term in the input field.
+      </p>
+      <p>
+        <Button variant="primary" href="https://unsplash.com" target="_blank">
+          Learn more
+        </Button>
+      </p>
+    </Jumbotron>
+  );
+};
+
+export default Welcome;
