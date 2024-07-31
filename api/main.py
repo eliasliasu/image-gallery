8eae238a1a00428116b1a68fb59c378b50ade159
Elias Liasu <eliasbabatunde@gmail.com>
2024-07-31 07:07:45 +0100

    enable pylint, black and pycodestyle for formatting

--- api/main.py
+++ api/main.py
@@ -8,31 +8,30 @@ load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
-DEBUDG=bool(os.environ.get("DEBUG", True))
+DEBUDG = bool(os.environ.get("DEBUG", True))
 
 
 if not UNSPLASH_KEY:
-  raise EnvironmentError("Please set your UNSPLASH_KEY in .env.local")
+    raise EnvironmentError("Please set your UNSPLASH_KEY in .env.local")
 
 app = Flask(__name__)
 CORS(app)
 
 app.config["DEBUG"] = DEBUDG
 
+
 @app.route("/new-image")
 def new_image():
-  word = request.args.get("query")
+    word = request.args.get("query")
 
-  headers = {
-    "Accept-Version": "v1",
-    "Authorization": "Client-ID " + UNSPLASH_KEY
-  }
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
 
-  params = {"query": word}
+    params = {"query": word}
 
-  response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
-  data =response.json()
-  return data
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=5050)
